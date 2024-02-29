b54f3678c3784e694d86b82dcc8ddd8982c84afb
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-02-29 16:01:22 -0300

    test 2

--- Calculator/src/test/java/DemoTest.java
+++ Calculator/src/test/java/DemoTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Test;
+
 public class DemoTest {
     @Test
     void testDemo() {
         System.out.println("Testing - SPC");
+    }
+    @Test
+    void calculate() {
+        System.out.println("Teste 2");
     }
 }
