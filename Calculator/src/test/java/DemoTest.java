31e65fca1701b15af2e905a4de8b30de96e8e70c
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-03-01 19:21:19 -0300

    calculator class

--- Calculator/src/test/java/DemoTest.java
+++ Calculator/src/test/java/DemoTest.java
@@ -1,6 +1,8 @@
+import org.example.Calculator;
 import org.junit.jupiter.api.Test;
 
 public class DemoTest {
+    Calculator calculate = new Calculator();
     @Test
     void testDemo() {
         System.out.println("Testing - SPC");
@@ -8,5 +10,9 @@ public class DemoTest {
     @Test
     void calculate() {
         System.out.println("Teste 2");
+    }
+    @Test
+    void somaTest(){
+
     }
 }
